7b5e729bd50c5f28806d780b346ab9112c9b3939
meertseker <meertseker@gmail.com>
2025-05-13 08:56:47 +0100

    env issue solved

---
+++ components/ChatBubble.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ChatBubble = () => {
+  return (
+    <View>
+      <Text>ChatBubble</Text>
+    </View>
+  )
+}
+
+export default ChatBubble
